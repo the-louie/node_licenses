38fab3cf68e7878ef9b93b48aef5d4ba30dc0943
Anders Green <anders.green@proceranetworks.com>
2015-10-31 17:07:32 +0100

    Restructure a bit and start of license info from LICENSE file

--- index.js
+++ index.js
@@ -24,55 +24,67 @@ function unique_licenses(component_licenses) {
 }
 
 
+function packagejson(filepath) {
+    try {
+        var fs = require("fs"); //Load the filesystem module
+        var stats = fs.statSync(filepath);
+        if(stats.size === 0)
+            return ;
 
+        var json = require((filepath).replace('//','/'));
+        if (json.license !== undefined) {
+            switch(typeof (json.license)) {
+                case "string":
+                    return {filepath: filepath, component: path.dirname(filepath).split('/').slice(-1)[0], license: json.license };
+                    break;
+                case "object":
+                    if (json.license.type !== undefined) { // object
+                        return {filepath: filepath, component: path.dirname(filepath).split('/').slice(-1)[0], license: json.license.type };
+                    }
+                    break;
+                default:
+                    return;
+
+            }
+        } else if (json.licenses !== undefined) {
+            if (typeof (json.licenses) === 'string') {
+                return {filepath: filepath, component: path.dirname(filepath).split('/').slice(-1)[0], license: json.licenses };
+            } else if (json.licenses[0] !== undefined) { // list
+                json.licenses.forEach(function(license) {
+                    return {filepath: filepath, component: path.dirname(filepath).split('/').slice(-1)[0], license: license.type };
+                });
+            }
+        } else {
+            return;
+        }
+    } catch (e) {
+        console.log("Error when loading", filepath);
+        console.log(e);
+        return;
+    }
+}
+
+function licensefile(filepath) {
+
+    return {filepath: filepath, component: path.dirname(filepath).split('/').slice(-1)[0], license: "textfile", licensetext: "" };
+}
 
 recursive(process.argv[2], function (err, files) {
     var component_licenses = [];
 
     files.forEach(function(filepath) {
-        if (path.basename(filepath) != "package.json")
-            return;
         if (filepath.split('/').indexOf('test') != -1)
             return;
 
-        // console.log(path.dirname(filepath).split('/').slice(-1)[0], path.basename(filepath));
-            try {
-                var fs = require("fs"); //Load the filesystem module
-                var stats = fs.statSync(filepath);
-                if(stats.size === 0)
-                    return ;
+        if (path.basename(filepath) == "package.json") {
+            // search package.json for a licence item
+            component_licenses.push(packagejson(filepath));
+        } else if (path.basename(filepath) == 'LICENSE') {
+            // just get the text from the LICENSE file
+            component_licenses.push(licensefile(filepath));
+        }
 
-                var json = require((filepath).replace('//','/'));
-                if (json.license !== undefined) {
-                    switch(typeof (json.license)) {
-                        case "string":
-                            component_licenses.push({filepath: filepath, component: path.dirname(filepath).split('/').slice(-1)[0], license: json.license });
-                            break;
-                        case "object":
-                            if (json.license.type !== undefined) { // object
-                                component_licenses.push({filepath: filepath, component: path.dirname(filepath).split('/').slice(-1)[0], license: json.license.type });
-                            }
-                            break;
-                        default:
-                            component_licenses.push({filepath: filepath, component: path.dirname(filepath).split('/').slice(-1)[0], license: "Unknown" });
-                    }
-                } else if (json.licenses !== undefined) {
-                    if (typeof (json.licenses) === 'string') {
-                        component_licenses.push({filepath: filepath, component: path.dirname(filepath).split('/').slice(-1)[0], license: json.licenses || "Unknown" });
-                    } else if (json.licenses[0] !== undefined) { // list
-                        json.licenses.forEach(function(license) {
-                            component_licenses.push({filepath: filepath, component: path.dirname(filepath).split('/').slice(-1)[0], license: license.type || "Unknown" });
-                        });
-                    }
-                } else {
-                    component_licenses.push({filepath: filepath, component: path.dirname(filepath).split('/').slice(-1)[0], license: "Unknown" });
-                    return;
-                }
-            } catch (e) {
-                console.log("Error when loading", filepath);
-                console.log(e);
-                return;
-            }
+
 
 
     });
